c4fdb0e9e9602299d1d901bc4512dee428d63619
Roland Hauser <holyroli82@gmail.com>
2015-07-30 11:40:09 +0200

    Added more javadoc

--- src/main/java/ch/sourcepond/maven/plugin/jenkins/proxy/ProxyFinder.java
+++ src/main/java/ch/sourcepond/maven/plugin/jenkins/proxy/ProxyFinder.java
@@ -18,14 +18,26 @@ import org.apache.maven.settings.Proxy;
 import org.apache.maven.settings.Settings;
 
 /**
- *
+ * Facade to determine a concrete {@link Proxy} instance with a specified id.
  */
 public interface ProxyFinder {
 
 	/**
+	 * Tries to find the appropriate {@link Proxy} instance for the proxy-id
+	 * specified. If the proxy-id is {@code null} the search will be cancelled
+	 * and {@code null} will be returned. If the proxy-id is not {@code null}
+	 * but not {@link Proxy} could be found, a {@link MojoExecutionException}
+	 * will be cause to be thrown.
+	 * 
 	 * @param pProxyIdOrNull
+	 *            Proxy-id or {@code null}
 	 * @param pSettings
-	 * @return
+	 *            Settings, must not be {@code null}
+	 * @return {@link Proxy} instance or {@code null} if proxy-id specified was
+	 *         {@code null}
+	 * @throws MojoExecutionException
+	 *             Thrown, if proxy-id was not {@code null} but no proxy could
+	 *             be found.
 	 */
 	Proxy findProxy(String pProxyIdOrNull, Settings pSettings)
 			throws MojoExecutionException;
